e3e0dcf2123349387ef0b4756560defd32f16ec6
GU9797 <mikegu97@gmail.com>
2025-03-24 21:25:14 -0400

    Remove redundant context

--- stepful-crud-app/src/index.js
+++ stepful-crud-app/src/index.js
@@ -3,16 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
-// React Context Provider
-import { AppControllerProvider } from "context";
-
 const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <AppControllerProvider>
       <App />
-    </AppControllerProvider>
   </BrowserRouter>
 );
